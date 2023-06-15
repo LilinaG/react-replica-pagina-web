acfece887ab4e234305ac5b79f0f83f8a5a78740
LilinaG <lgutierrezfer@gmail.com>
2023-06-15 19:03:53 +0200

    navbar

--- src/components/atoms/Navbar.jsx
+++ src/components/atoms/Navbar.jsx
@@ -1,28 +1,24 @@
 import NonColorButton from "./NonColorButton";
 import './navbar.css';
-import './nonColorButton.css';
-import logo from '../assets/images/logo.svg';
+import logo from '../../assets/images/logo.svg';
+import { Link } from "react-router-dom";
+
 
 function Navbar() {
     return (
     <>
-     <header> 
-        <div className="navbar-logo"><img src= {logo}alt='Logo'/></div>
-
-        <nav className="menu">
-            <a href="#description">DESCRIPTION</a>
-            <a href="#section-tracking">FEATURES</a>   
-            <a href="#container-icons-numbers">SCREENS</a>   
-            <a href="#">EXTRA</a>
-            <img src="../src/assets/images/down-arrow.png" className="arrow"/>
+    <header className="header-container">
+        <nav>
+            <img src={logo} className="navbar-logo"/>
+            <ul className="menu">
+                <Link to='/Description'><li>DESCRIPTION</li></Link>
+                <Link to='/Tracking'><li>FEATURES</li></Link>
+                <Link to='/UserTerstimonials'><li>SCREENS</li></Link>
+                <Link to='/Numbers'><li>EXTRA</li></Link>
+                <li><a href="https://factoriaf5.org"/><NonColorButton>DOWNLOAD</NonColorButton></li>
+            </ul>
         </nav>
-
-        <div className="download">
-            <NonColorButton button_content={'DOWNLOAD'}></NonColorButton>
-        </div> 
     </header>
-       
-
     </>
       
     );
